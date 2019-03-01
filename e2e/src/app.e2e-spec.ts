74d4fd82a401e29a2e80f89591a5ed221f87d092
Christopher <24984762+ChristopherKiss@users.noreply.github.com>
2019-03-02 07:56:56 +1100

    feat: initial project setup

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,31 @@
-import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+    let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    beforeEach(() => {
+        page = new AppPage();
+    });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to insight!');
-  });
+    it('should display welcome message', async () => {
+        await page.navigateTo();
+        await expect(page.getTitleText()).toEqual('Welcome to insight!');
+    });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+    afterEach(async () => {
+        // Assert that there are no errors emitted from the browser
+        const logs = await browser
+            .manage()
+            .logs()
+            .get(logging.Type.BROWSER);
+
+        const loggingEntry = {
+            level: logging.Level.SEVERE
+        } as logging.Entry;
+
+        const expectedLoggingSeverity = jasmine.objectContaining(loggingEntry);
+
+        expect(logs).not.toContain(expectedLoggingSeverity);
+    });
 });
