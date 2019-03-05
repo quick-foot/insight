7f7ccec765fa45674daaa8fc786cae0fc1c9c810
Christopher <24984762+ChristopherKiss@users.noreply.github.com>
2019-03-06 06:13:31 +1100

    feat: add angular material

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,6 +1,7 @@
+// tslint:disable:ordered-imports
+import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
